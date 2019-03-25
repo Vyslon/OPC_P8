36b6b573cab59a26abbbd880d1082b49711bce35
Thomas SANTONI <thomassantoni@MacBook-Pro-de-Thomas.local>
2019-03-25 09:41:20 +0100

    fixing settings.py

--- pur_beurre_platform/settings.py
+++ pur_beurre_platform/settings.py
@@ -16,16 +16,19 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
+
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(PROJECT_ROOT, '../Substitute_Platform/static'),
     )
 
 
